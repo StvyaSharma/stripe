97ff65e7243156bbaa940aa8c9c2a23029df0fae
Stvya Sharma <ssha9076@uni.sydney.edu.au>
2023-12-04 18:08:36 +1100

    minor changes

--- src/app/Hero.jsx
+++ src/app/Hero.jsx
@@ -39,8 +39,32 @@ function Hero() {
             </div>
 
             <div className="hero-pictures">
-                <img className="desktop" src={'/desktop.jpg'} alt="desktop" />
-                <img className="phone" src={'/phone1.png'} alt="phone" />
+                <div style={{ position: 'absolute', top: '-200px', right: '-700px', width:'1198px',height:'811px' ,zIndex:1}}>
+                    <Image
+                        src={desktop}
+                        alt="desktop"
+                        width={1198}
+                        height={811}
+                        style={{
+                            borderRadius: '1.2rem',
+                            boxShadow: '90px 90px 150px -70px grey',
+                            transform: 'scale(0.6)',
+                        }}
+                    />
+                </div>
+                <div style={{ position: 'absolute', top: '-300px', right: '-100px', width:'581px',height:'1190px',zIndex:'2' }}>
+                    <Image
+                        src={phone}
+                        alt="phone"
+                        width={581}
+                        height={1190}
+                        style={{
+                            borderRadius: '7rem',
+                            boxShadow: '0 0 200px -10px grey',
+                            transform: 'scale(0.4)',
+                        }}
+                    />
+                </div>
             </div>
         </section>
     );
