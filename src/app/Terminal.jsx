a8a2e539cdd749565d870b5c6fb8ab1480ebd86e
Stvya Sharma <ssha9076@uni.sydney.edu.au>
2023-12-04 20:53:06 +1100

    Minor changes

--- src/app/Terminal.jsx
+++ src/app/Terminal.jsx
@@ -11,16 +11,16 @@ function Terminal() {
     return (
 
 
-        <div className="terminal-content" style={{backgroundColor:''}}>
-            <div className="terminal-upper" style={{display:'flex'}}>
-                <div className="upper-text"  style={{ zIndex: '10'}}>
-                    <span className="terminal-head" style={{ zIndex: '10'}}>
+        <div className="terminal-content" style={{ backgroundColor: '' }}>
+            <div className="terminal-upper" style={{ display: 'flex' }}>
+                <div className="upper-text" style={{ zIndex: '10' }}>
+                    <span className="terminal-head" style={{ zIndex: '10' }}>
                         Global scale
                     </span>
-                    <h1 className="terminal-title"  style={{ zIndex: '10'}}>
+                    <h1 className="terminal-title" style={{ zIndex: '10' }}>
                         The backbone for global commerce
                     </h1>
-                    <p className="terminal-desc"  style={{ zIndex: '10'}}>
+                    <p className="terminal-desc" style={{ zIndex: '10' }}>
                         Stripe makes moving money as easy and programmable as moving data. Our teams are based in offices around the world and we process hundreds of billions of dollars each year for ambitious businesses of all sizes.
                     </p>
 
@@ -31,7 +31,7 @@ function Terminal() {
             </div>
 
             <div className="terminal-details">
-                <div className="box" style={{ zIndex: '10',width:'100%' }}>
+                <div className="box" style={{ zIndex: '10', width: '100%' }}>
 
                     <h4 className="head">250+</h4>
                     <p className="desc">
@@ -42,7 +42,7 @@ function Terminal() {
 
                 </div>
 
-                <div className="box" style={{ zIndex: '10',width:'100%' }}>
+                <div className="box" style={{ zIndex: '10', width: '100%' }}>
 
                     <h4 className="head">99.999%</h4>
                     <p className="desc">
@@ -55,7 +55,7 @@ function Terminal() {
 
                 </div>
 
-                <div className="box" style={{ zIndex: '10',width:'100%' }}>
+                <div className="box" style={{ zIndex: '10', width: '100%' }}>
 
                     <h4 className="head">47+</h4>
                     <p className="desc">
@@ -66,7 +66,7 @@ function Terminal() {
 
                 </div>
 
-                <div className="box" style={{ zIndex: '10',width:'100%' }}>
+                <div className="box" style={{ zIndex: '10', width: '100%' }}>
 
                     <h4 className="head">135+</h4>
                     <p className="desc">
