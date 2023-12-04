a0562bbd552a5ba77379e54d0ba32f8f40127b93
Stvya Sharma <ssha9076@uni.sydney.edu.au>
2023-12-04 12:20:42 +1100

    updaed

--- src/app/main/GradientComponent.js
+++ src/app/main/GradientComponent.js
@@ -15,7 +15,7 @@ const GradientComponent = () => {
 
     return (
         <div className='canvas'>
-            <canvas id="gradient-canvas" data-transition-in style={{ width: '100%', height: '100%' }} />
+            <canvas id="gradient-canvas" data-js-darken-top  data-transition-in style={{ width: '100%', height: '100%' }} />
         </div>
     );
 };
